893ef3195f8ac0e1835ac5e2800c3512fe83cc4d
Muhammad Aqil <aqilfahruddin01@gmail.com>
2025-03-09 23:04:11 +0800

    Add TestNG & simple action

--- src/test/java/TestNGExample.java
+++ src/test/java/TestNGExample.java
@@ -1,14 +1,43 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestNGExample {
-    @Test
-    public void testGoogleTitle() {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp (){
         System.setProperty("webdriver.chrome.driver", "C://Selenium//WebDriver//chromedriver134.exe/");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @Test
+    public void LoginPage() {
+
+        String firstProduct, productDetails;
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+//      Click first item
+        driver.findElement(By.id("item_4_title_link")).click();
+
+        firstProduct = driver.findElement(By.className("inventory_details_name")).getText();
+        productDetails = driver.findElement(By.className("inventory_details_desc")).getText();
+
         System.out.println("Page title: " + driver.getTitle());
+        System.out.println("Product : " + firstProduct);
+        System.out.println("Product Details : " + productDetails);
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
         driver.quit();
     }
+
+
 }
