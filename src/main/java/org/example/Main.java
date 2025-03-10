b5015222f0bf879f57def396ba580992ecf3845b
Muhammad Aqil <aqilfahruddin01@gmail.com>
2025-03-10 23:57:23 +0800

    try assert & verify

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -16,5 +19,31 @@ public class Main {
 
         // Close the browser
         driver.quit();
+    }
+
+    public void LoginPage (){
+        //Initialize Web Driver
+        System.setProperty("webdriver.chrome.driver", "C://Selenium//WebDriver//chromedriver134.exe/");
+        WebDriver driver = new ChromeDriver();
+
+        //Redirect to Sauce Lab site
+        driver.get("https://www.saucedemo.com/");
+
+        //Key In login credential
+        String currentURL;
+        String expectedURL = "https://www.saucedemo.com/inventory.html";
+        String username = "standard_user";
+        String password = "secret_sauce";
+        driver.findElement(By.id("user-name")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("login-button")).click();
+        currentURL = driver.getCurrentUrl();
+//        Assert.isTrue(currentURL,expectedURL);
+
+
+
+
+
+
     }
 }
--- src/test/java/TestNGExample.java
+++ src/test/java/TestNGExample.java
@@ -1,7 +1,13 @@
+import com.sun.source.tree.IfTree;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.util.function.Function;
+
+import static org.testng.Assert.assertEquals;
 
 public class TestNGExample {
     WebDriver driver;
@@ -17,27 +23,43 @@ public class TestNGExample {
     public void LoginPage() {
 
         // Initialize variable
-        String firstProduct, productDetails;
+        String firstProduct, productDetails, currentUrl, expectedURL, error;
+        String username = "standard_user";
+        String password = "secret_sauce1";
 
         // Key in Login credential
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("user-name")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.id("login-button")).click();
 
+        currentUrl = driver.getCurrentUrl();
+        expectedURL = "https://www.saucedemo.com/inventory.html";
+//        Assert.assertEquals(currentUrl,expectedURL);
+
+        if (!currentUrl.contains(expectedURL)){
+            //Verify error message appear
+//            error = driver.findElement(By.xpath("//form/div[3]/h3[contains(@data-test,'error')]")).getText();
+            System.out.println(driver.findElement(By.xpath("//form/div[3]/h3[contains(@data-test,'error')]")).getText());
+        }
+
+
+        //If Error, it
+
+
         // Click first item
-        driver.findElement(By.id("item_4_title_link")).click();
+//        driver.findElement(By.id("item_4_title_link")).click();
 
         // Need to verify the page loaded is correct
         // --------
 
         // Get Current Product name & details
-        firstProduct = driver.findElement(By.className("inventory_details_name")).getText();
-        productDetails = driver.findElement(By.className("inventory_details_desc")).getText();
-
-        // Display in report
-        System.out.println("Page title: " + driver.getTitle());
-        System.out.println("Product : " + firstProduct);
-        System.out.println("Product Details : " + productDetails);
+//        firstProduct = driver.findElement(By.className("inventory_details_name")).getText();
+//        productDetails = driver.findElement(By.className("inventory_details_desc")).getText();
+//
+//        // Display in report
+//        System.out.println("Page title: " + driver.getTitle());
+//        System.out.println("Product : " + firstProduct);
+//        System.out.println("Product Details : " + productDetails);
 
     }
 
